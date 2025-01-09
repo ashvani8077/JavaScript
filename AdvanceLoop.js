398263c3e392dbe64ac82e3c46666360d650f42a
Ashvani Sharma <panditashuabvp@gmail.com>
2025-01-09 21:31:47 +0530

    js

---
+++ AdvanceLoop.js
@@ -0,0 +1,18 @@
+
+// for of
+
+const arr=[1,2,3,4,5]
+for (const i of arr) {
+    console.log(i);
+    
+}
+
+//for in
+
+
+//for each loop
+const coding=["js","ruby","python","cpp","java"];
+
+coding.forEach(function(item){
+    console.log(`foreach ${item}`)
+})
---
+++ FilterMapReduce.js
@@ -0,0 +1,30 @@
+
+//Filter
+
+
+const myNums=[1,2,3,4,5,6,7,8];
+// const newNums=myNums.filter((num)=>{
+//     return num>4;
+// });
+// console.log(newNums);
+
+
+//map
+
+
+// const newNums=myNums.map((num)=>{
+//     return num+4;
+// })
+// console.log(newNums);
+
+
+// const newNums=myNums.map((num)=> num*10).map((num)=>num+1);
+// console.log(newNums);
+
+
+//reduce
+
+// const a=myNums.reduce(function(acc, curr){
+//     return acc+curr;
+// },0);
+// console.log(a);
